111ffad0d51e7e6acbf351eaa5d1e412366eeae1
mrveldkamp <colin.veldkamp@epsb.ca>
2020-04-02 19:45:39 +0000

    paragraph random colors

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("click", run);
 function run() {
   document.body.style.background = randomColor();
   document.querySelector("h1").style.color = randomColor();
+  document.querySelector("p").style.color = randomColor();
 }
 
 function randomColor() {
