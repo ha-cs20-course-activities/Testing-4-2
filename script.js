e08da4349604dc5c95f4581acbc7d0483480105c
mrveldkamp <colin.veldkamp@epsb.ca>
2020-04-02 19:44:29 +0000

    random colors

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", run);
+
+function run() {
+  document.body.style.background = randomColor();
+  document.querySelector("h1").style.color = randomColor();
+}
+
+function randomColor() {
+  return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+}
